5469a8c6133480647392af232386a634ab7c410c
Minh Danh <minhdanhdn@gmail.com>
2026-05-25 22:15:25 +0700

    feat: define main models (Product, Order, OrderItem)

---
+++ src/main/java/com/example/springecom/model/OrderItem.java
@@ -0,0 +1,30 @@
+package com.example.springecom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    int quantity;
+    private BigDecimal totalPrice;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Order order;
+}
