2dbf46adeed6cea520751013c845e60f7896d0eb
Minh Danh <minhdanhdn@gmail.com>
2026-05-25 22:18:23 +0700

    feat: add repostory layer for database connection

---
+++ src/main/java/com/example/springecom/repo/OrderRepo.java
@@ -0,0 +1,11 @@
+package com.example.springecom.repo;
+
+import com.example.springecom.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+
+
+}
