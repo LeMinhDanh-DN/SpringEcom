87feeb70ef1b570d9d328f962b4892b5a1bc50df
Minh Danh <minhdanhdn@gmail.com>
2026-05-25 23:25:52 +0700

    feat: add user entity for storage users's acc/pass

---
+++ src/main/java/com/example/springecom/model/User.java
@@ -0,0 +1,22 @@
+package com.example.springecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+}
